420b8ac0b1e5aff712e149a38ef99a9bb6bbec33
Roberto Duarte <103781917+robertoduartejr@users.noreply.github.com>
2022-07-27 15:14:33 -0300

    Inital GUI almost finished

--- main.py
+++ main.py
@@ -1,31 +1,54 @@
 from tkinter import *
+from datetime import datetime
 
+now = datetime.now()
+current_time = now.strftime("%H:%M:%S")
 
 #global variables
 flag = x1 = y1 = 0
+user_name = ""
+user_id = ""
+
 
 #CHAT WINDOW
-def chat():
-    chat_window = Tk()
-    chat_window.title("Company Chat")
-    chat_window.iconbitmap(default="chatimg.ico")
-    chat_window.geometry("816x597+214+23")
-    chat_window.resizable(width=False, height=False)
+def send(user_name, user_id, user_text,T,T2):
 
-    T = Text(chat_window, height=5, width=52)
-    Fact = """A man can be arrested in
-    Italy for wearing  arrested in
-    Italy for wearing a skirt in public."""
+    #First internal part
+    #edited text to send:
+    now = datetime.now()
+    current_time = now.strftime("%H:%M")
+    edited_text = f'\n{current_time} {user_name} {user_id} says: {user_text}'
+    T.insert(END, edited_text)
+    print(user_text)
+    T2.delete("1.0","end")
+
+    #Now online part
+
+
+def chat(user_name, user_id):
+    #chat_window = Tk()
+    main_window.title("Company Chat")
+    main_window.iconbitmap(default="chatimg.ico")
+    main_window.geometry("816x597+214+23")
+    main_window.resizable(width=False, height=False)
+    print(user_id,user_name,"ok")
+
+    #conversation box
+    T = Text(main_window, height=5, width=52)
     T.place(width= 600, height = 389, x= 3 , y= 3)
-    T.insert(END, Fact)
-    T.config(state=DISABLED)
+    #T.config(state=DISABLED)
+    print(T.get("1.0",'end-1c')) # a way to get information from a text box
 
-    T2 = Text(chat_window, height=5, width=52)
+    # message box
+    T2 = Text(main_window, height=5, width=52)
     T2.place(width = 600, height = 140, x = 3, y = 415)
 
+    #send button
+    send_button = Button(main_window, text="Send", command= lambda: send(user_name,user_id,T2.get("1.0",'end-1c'),T,T2))
+    send_button.place(width= 143, height = 33, x= 459 , y= 560)
 
-
-    chat_window.mainloop()
+    main_window.bind("<Button-1>", clique_esquerdo_mouse)
+    main_window.mainloop()
 
 #INITIAL WINDOW AND PARAMETERS
 main_window = Tk()
@@ -49,7 +72,8 @@ employee_id.place(width=113,height=14,x=6,y=93)
 employee_id.grid(sticky=W,pady=20)
 
 #Creating and position button
-main_button = Button(main_window, text = "Start Chat", command = chat)
+#lambda used so we can send args within the function
+main_button = Button(main_window, text = "Start Chat", command = lambda: chat(name.get(),user_id.get()))
 main_button.place(width=93,height=30,x=106,y=439)
 
 #Creating entry boxes
@@ -64,7 +88,7 @@ user_id.place(width= 171, height = 30, x= 96 , y= 141)
 
 
 
-#Pegar posição do cursos
+#get mouse position
 def clique_esquerdo_mouse(arg):
     global flag, x1, y1
     flag = not flag
