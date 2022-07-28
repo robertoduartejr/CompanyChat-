8d933a4ba4e2f6251ea68f0e1092861fdeb6a72a
Roberto Duarte <103781917+robertoduartejr@users.noreply.github.com>
2022-07-28 14:57:20 -0300

    v1 with all used functions

---
+++ ChatServer.py
@@ -0,0 +1,32 @@
+import socket
+import threading
+
+HOST = 'localhost'
+PORT = 7777
+i = 0 # client identifier
+clients = []
+
+def messagesTreatment(clients,i):
+    print("iniciou thread")
+    while True:
+        #receive the message and send to the other peer.
+        data = clients[i].recv(1024)
+        if i == 0:
+            clients[1].sendall(data)
+        if i == 1:
+            clients[0].sendall(data)
+
+#CREATING SOCKET
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen() #listening to new conncetions
+
+#loop to receive new connections and create threads for every new connection
+while True:
+    print("aguardando conexao de outra pessoa")
+    conn, ender = s.accept() #accept connection
+    clients.append(conn)
+    print("contectado em", ender)
+    thread = threading.Thread(target=messagesTreatment,args=[clients,i])
+    i=i+1
+    thread.start()
